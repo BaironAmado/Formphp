79e35566b00f0e6ab1dab2c3202393f1cefc372d
BaironAmado <baironildover@gmail.com>
2024-08-06 15:05:55 -0500

    informacion.php

--- informacion.php
+++ informacion.php
@@ -1,1 +1,10 @@
-
+<?php
+$identificacion= $_POST ['identificacion'];
+$nomapellidos=$post['nomapellidos'];
+$cargo=$_post ['salario'];
+$salario=$_post ['salario']
+  echo "La identificacion Empleado es: ".identificacion;
+echo "Los nombres y apellidos del empleado son: ".nomapellidos;
+echo "El cargo del empleado es: ".cargo;
+echo "El salario del empleado es: ".salario;
+?>
