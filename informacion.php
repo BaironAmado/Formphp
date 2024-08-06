925d49007a4dcd9d77e9c3e43b4e649d7a33888f
BaironAmado <baironildover@gmail.com>
2024-08-06 15:20:35 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,10 +1,10 @@
 <?php
-$Identidicacion=$_POST["identificacion"];
-$NombreApellidos=$_POST["nomapellido"];
-$Cargo=$_POST ["cargo"];
-$Salario=$_POST ["salario"];
+$Identidicacion=$_POST["Identificacion"];
+$NombreApellidos=$_POST["NomApellido"];
+$Cargo=$_POST ["Cargo"];
+$Salario=$_POST ["Salario"];
 echo "La idetifiacion del Empleado es: ".Identidicion;
-echo "El nombre y apellido del empleado es: ".Nomapellido;
+echo "El nombre y apellido del empleado es: ".NomApellido;
 echo "El cargo del empleado es:".Cargo;
 echo"El salario del empleado es: ".Salario;
 ?>
